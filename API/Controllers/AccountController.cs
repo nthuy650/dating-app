f64d091594551c7f9d53067fbbd005deff7dca60
nthuy650 <nthuy.wd@gmail.com>
2022-10-15 21:19:08 +0700

    Add token service (JWT)

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -39,6 +39,31 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(UserLoginDto userDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == userDto.Username);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(userDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid password");
+                }
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,21 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace API.Services;
+
+public class TokenService : ITokenService
+{
+    private readonly SymmetricSecurityKey _key;
+
+    public TokenService(IConfiguration config)
+    {
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+    }
+
+    public string CreateToken(AppUser user)
+    {
+        throw new NotImplementedException();
+    }
+}
