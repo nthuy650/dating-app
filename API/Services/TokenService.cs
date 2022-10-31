c997d95c8d0d780bb1e2de5681a4cbcac9dca998
nthuy650 <nthuy.wd@gmail.com>
2022-10-31 15:46:03 +0100

    adding logic for createToken

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,6 +1,8 @@
 ﻿using API.Entities;
 using API.Interfaces;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace API.Services;
@@ -16,6 +18,24 @@ public class TokenService : ITokenService
 
     public string CreateToken(AppUser user)
     {
-        throw new NotImplementedException();
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+        };
+
+        var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
     }
 }
